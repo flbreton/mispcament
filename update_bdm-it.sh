a665d05b2f717cc9934a69a85fddcd3f6c27b876
Florent Breton <florent.breton@laposte.net>
2022-02-24 11:08:49 +0100

    beta 3
    check du fichier json genere avec jq si jq est present

--- update_bdm-it.sh
+++ update_bdm-it.sh
@@ -19,7 +19,14 @@ which dbview >/dev/null  || perror "dbview absent"
 which awk >/dev/null  || perror "awk absent"
 which curl >/dev/null  || perror "curl absent"
 which wget >/dev/null  || perror "wget absent"
-
+# prequis facultatif
+which jq >/dev/null 
+if [ $? -eq 0 ] ; then 
+	check=1
+else
+	check=0
+	echo jq absent les fichiers json ne pourront pas etre valides >&2
+fi
 
 # Telechargement du dernier fichier sur le site d'amelie
 dbfile=`curl http://www.codage.ext.cnamts.fr/codif/bdm_it/index_tele_ucd.php 2>/dev/null |grep ucd_total -a | cut -d\" -f2|sed 's/.*\///'`
@@ -34,15 +41,34 @@ fi
 version=`echo $dbfile | cut -d_ -f3 | sed 's/^0*//'`
 
 # conversion du fichier dbf en json pour le cluster
+if [ $check -eq 1 ] ; then 
+	mv $clusterdir/$jsonfile $clusterdir/$jsonfile.sav
+fi
+
 dbview -b $dbfile | cut -d: -f1,7,3,11,38,39| sed 's/   *//g'|sort |uniq | iconv -f ISO885915 -t UTF8 | awk -v vers=$version -f $convertfile > $clusterdir/$jsonfile
 if [ $? -eq 0 ] ; then 
-	echo fichier $clusterdir/$jsonfile genere
+	if [ $check -eq 1 ] ; then 
+		jq -e . $clusterdir/$jsonfile >/dev/null 2>&1
+		if [ $? -eq 0 ] ; then 
+			echo fichier $clusterdir/$jsonfile genere
+			rm $clusterdir/$jsonfile.sav
+		else
+			echo le fichier genere n ete pas valide je remet l ancienne version
+			rm $clusterdir/$jsonfile
+			mv $clusterdir/$jsonfile.sav $clusterdir/$jsonfile
+		fi
+	else
+		echo fichier $clusterdir/$jsonfile genere
+	fi
 else
 	echo la conversion vers $clusterdir/$jsonfile a echoue
 	exit 3
 fi
 
 # generation du fichier galaxie
+if [ $check -eq 1 ] ; then 
+	mv $galaxiedir/$jsonfile $galaxiedir/$jsonfile.sav
+fi
 echo '{
   "description": "Liste Medicamant BDM-IT",
   "icon": "industry",
@@ -53,7 +79,19 @@ echo '{
   "version": 1
 }'>$galaxiedir/$jsonfile
 if [ $? -eq 0 ] ; then 
-	echo fichier $galaxiedir/$jsonfile genere
+	if [ $check -eq 1 ] ; then 
+		jq -e . $galaxiedir/$jsonfile >/dev/null 2>&1
+		if [ $? -eq 0 ] ; then 
+			echo fichier $galaxiedir/$jsonfile genere
+			rm $galaxiedir/$jsonfile.sav
+		else
+			echo le fichier genere n ete pas valide je remet l ancienne version
+			rm $galaxiedir/$jsonfile
+			mv $galaxiedir/$jsonfile.sav $galaxiedir/$jsonfile
+		fi
+	else
+		echo fichier $galaxiedir/$jsonfile genere
+	fi
 else
 	echo la conversion vers $galaxiedir/$jsonfile a echoue
 	exit 3
